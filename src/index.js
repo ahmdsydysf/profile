1f20ed44def24a09c6a5fb9ad6bda05ae39d2ebe
Ah-Sy-Yf <a.sayed.xc@gmail.com>
2023-11-26 04:01:32 +0200

    profile-react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+function App() {
+  const dataOfSkills = [
+    { nm: "HTML", clr: "blue" },
+    { nm: "CSS", clr: "red" },
+    { nm: "Javascript", clr: "yellow" },
+    { nm: "PHP", clr: "skyblue" },
+    { nm: "Laravel", clr: "darkred" },
+    { nm: "Ajax", clr: "blueviolet" },
+    { nm: "API", clr: "aquamarine" },
+    { nm: "Bootstrap", clr: "burlywood" },
+    { nm: "JQuery", clr: "greenyellow" },
+    { nm: "UI & UX", clr: "indianred" },
+  ];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="card main-card col-3">
+          <div className="row">
+            <MainPhoto img="images/picofme (2).png" />
+            <Intro
+              lorem="Some quick example text to build on the card title and make up the bulk
+        of the card's content. Some quick example text to build on the card
+        title and make up the bulk of the card's content. Some quick example
+        text to build on the card title and make up the bulk of the card's"
+            />
+            <Skills skl={dataOfSkills} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MainPhoto(props) {
+  return (
+    <div className="">
+      <img src={props.img} className="card-img-top" />
+    </div>
+  );
+}
+
+function Intro(props) {
+  return (
+    <div class="card-body">
+      <p class="card-text">{props.lorem}</p>
+    </div>
+  );
+}
+
+function Skills(props) {
+  console.log(props.skl);
+  return (
+    <div class="card-footer">
+      {props.skl.map((elm) => (
+        <span class="badge mx-1" style={{ backgroundColor: elm.clr }}>
+          {elm.nm}
+        </span>
+      ))}
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
